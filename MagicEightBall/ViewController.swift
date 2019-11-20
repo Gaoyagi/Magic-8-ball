927dac220e9dbd028b9b693ed2949f4b74631d9a
George Aoyagi <aoyagigeorge@gmail.com>
2019-11-20 13:18:26 -0800

    Initial Commit

---
+++ MagicEightBall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MagicEightBall
+//
+//  Created by George Aoyagi on 11/20/19.
+//  Copyright © 2019 George Aoyagi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
